318a28b2748e599744090a54e32a0515c24bdc0f
wanderer911 <dp160291sas@gmail.com>
2019-08-16 16:01:49 +0300

    changed to req.body

--- serverless/andriis/serverless.js
+++ serverless/andriis/serverless.js
@@ -1,17 +1,17 @@
 module.exports = functionsBuilder =>
   functionsBuilder
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-        const {userId} = req.query
+      const { userId } = req.body;
       const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPremiumInfo', userId);
       return result;
     })
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-        const {userId} = req.query
+      const { userId } = req.body;
       const result = await ctx.legacyRpcClient('PlansBoApi', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPlanPackages ', userId);
       return result;
     })
     .addWebFunction('GET', '/hello', async (ctx, req) => {
-      return {hello: 'world'};
+      return { hello: 'world' };
     });
