0901db4dda1604c54204ae56350bae92effb7f0a
wanderer911 <dp160291sas@gmail.com>
2019-08-16 16:24:29 +0300

    keeping query

--- serverless/andriis/serverless.js
+++ serverless/andriis/serverless.js
@@ -1,19 +1,13 @@
 module.exports = functionsBuilder =>
   functionsBuilder
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-      try {
-        const userId = req.body.userId;
-
-        const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
-          .invoke('getUserPremiumInfo', userId);
-        return result;
-      } catch (e) {
-        return e;
-      }
-
+      const { userId } =  req.query;
+      const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
+        .invoke('getUserPremiumInfo', userId);
+      return result;
     })
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-      const { userId } = req.body;
+      const { userId } =  req.query;
       const result = await ctx.legacyRpcClient('PlansBoApi', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPlanPackages ', userId);
       return result;
