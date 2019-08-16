d0487d6ed80e3496256cb505d6ba980a30b15c48
wanderer911 <dp160291sas@gmail.com>
2019-08-16 15:48:57 +0300

    'initial'

---
+++ serverless.js
@@ -0,0 +1,14 @@
+module.exports = functionsBuilder =>
+  functionsBuilder
+    .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
+        const {userId} = req.query
+      const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
+        .invoke('getUserPremiumInfo', userId);
+      return result;
+    })
+    .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
+        const {userId} = req.query
+      const result = await ctx.legacyRpcClient('PlansBoApi', 'com.wixpress.premium.premium-plans')
+        .invoke('getUserPlanPackages ', userId);
+      return result;
+    })
