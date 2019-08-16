511e06ea130c0a07621bf5f35ea02137769f3db7
wanderer911 <dp160291sas@gmail.com>
2019-08-16 16:10:02 +0300

    stopped the test. keeping the query

--- serverless/andriis/serverless.js
+++ serverless/andriis/serverless.js
@@ -1,20 +1,17 @@
 module.exports = functionsBuilder =>
   functionsBuilder
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-      const { userId } = await req.body.json();
+      const { userId } =  req.query;
       const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPremiumInfo', userId);
       return result;
     })
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-      const { userId } = await req.body.json();
+      const { userId } =  req.query;
       const result = await ctx.legacyRpcClient('PlansBoApi', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPlanPackages ', userId);
       return result;
     })
     .addWebFunction('GET', '/hello', async (ctx, req) => {
-      return req;
-    })
-    .addWebFunction('POST', '/hello', async (ctx, req) => {
       return req;
     });
