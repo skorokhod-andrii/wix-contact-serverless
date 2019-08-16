2eee112961f7adf6ce8cd2cdbd498736c7ad6de0
wanderer911 <dp160291sas@gmail.com>
2019-08-16 16:28:58 +0300

    added test function hello

--- serverless/andriis/serverless.js
+++ serverless/andriis/serverless.js
@@ -1,17 +1,17 @@
 module.exports = functionsBuilder =>
   functionsBuilder
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-        const {userId} = req.body;
+      const { userId } = req.query;
       const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPremiumInfo', userId);
       return result;
     })
     .addWebFunction('POST', '/getUserPlanPackages', async (ctx, req) => {
-        const {userId} = req.body;
+      const { userId } = req.query;
       const result = await ctx.legacyRpcClient('PlansBoApi', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPlanPackages ', userId);
       return result;
     })
     .addWebFunction('GET', '/hello', async (ctx, req) => {
-      return {hello: 'world'};
+      return { hello: 'world' };
     });
