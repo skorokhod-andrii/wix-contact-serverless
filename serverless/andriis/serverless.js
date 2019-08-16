754e58d0a9f4be4a68c7f7cffc647f49118a316b
wanderer911 <dp160291sas@gmail.com>
2019-08-16 16:22:32 +0300

    changed folder structure

--- serverless/andriis/serverless.js
+++ serverless/andriis/serverless.js
@@ -1,13 +1,19 @@
 module.exports = functionsBuilder =>
   functionsBuilder
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-      const { userId } =  req.body;
-      const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
-        .invoke('getUserPremiumInfo', userId);
-      return result;
+      try {
+        const userId = req.body.userId;
+
+        const result = await ctx.legacyRpcClient('PlansBoUserManager', 'com.wixpress.premium.premium-plans')
+          .invoke('getUserPremiumInfo', userId);
+        return result;
+      } catch (e) {
+        return e;
+      }
+
     })
     .addWebFunction('POST', '/getUserPremiumInfo', async (ctx, req) => {
-      const { userId } =  req.body;
+      const { userId } = req.body;
       const result = await ctx.legacyRpcClient('PlansBoApi', 'com.wixpress.premium.premium-plans')
         .invoke('getUserPlanPackages ', userId);
       return result;
